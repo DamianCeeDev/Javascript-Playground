70482ee8e13d44e588125c2dfa012deac8ccb7c9
DamianCeeDevelops <rdcorbo555@gmail.com>
2024-04-19 00:09:22 -0400

    Just practicing some simple Javascript stuff

---
+++ Javascript Practice/test.js
@@ -0,0 +1,35 @@
+let firstName = "Damian";
+
+console.log(firstName);
+
+const namesofPeople = ['Paul', 'Danny', 'Roger', 'Bob', 'Person Thing'];
+
+console.log(namesofPeople);
+
+for (i = 0; i <= 5; i++) {
+    console.log(i);
+}
+
+namesofPeople.push('Dale');
+
+console.log(namesofPeople);
+
+namesofPeople.forEach(function (namesofPeople) {
+    console.log(namesofPeople);
+})
+
+
+function sayHello(name) {
+    console.log("Hello " + name + ".");
+}
+
+sayHello("Damian");
+
+
+const monsters = {
+    name: "hairy monster",
+    powers: "Very hairy",
+    alive: true
+};
+
+console.log(monsters);
