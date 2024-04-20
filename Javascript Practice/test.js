cfb2c8548abde612a9e215e036d0ef753b76b6f2
DamianCeeDevelops <rdcorbo555@gmail.com>
2024-04-20 00:30:39 -0400

    Added more meaningless code for the sake of experimenting with objects and seeing how I can implement this as a battle system in an interactive project in the future.

--- Javascript Practice/test.js
+++ Javascript Practice/test.js
@@ -27,17 +27,17 @@ function sayHello(name) {
 sayHello("Damian");
 
 
-let monsters = {
+let otherMonsters = {
     name: "hairy monster",
     powers: "Very hairy",
     alive: true
 };
 
-console.log(monsters);
-console.log(monsters.alive);
+console.log(otherMonsters);
+console.log(otherMonsters.alive);
 
-monsters.powers = "being really smell";
-console.log(monsters);
+otherMonsters.powers = "being really smell";
+console.log(otherMonsters);
 
 const country = "USA";
 const continent = "North America";
@@ -88,4 +88,43 @@ console.log(Number('19') - Number('13') + Number('17')); // -> ?
 console.log(Number('19') - Number('13') + 17); // -> ?
 console.log(Number('123') < 57); // -> ?
 console.log(5 + 6 + Number('4') + 9 - 4 - 2); // -> ?
+
+const monsters = {
+    monster1: {
+        name: "big monster",
+        life: 500,
+        attack: 23
+    },
+    monster2: {
+        name: "small monster",
+        life: 300,
+        attack: 15
+    },
+    monster3: {
+        name: "medium monster",
+        life: 400,
+        attack: 18
+    }
+};
+
+
+if (monsters.monster1.attack > monsters.monster2.attack) {
+    console.log(`${monsters.monster1.name} attacks harder than ${monsters.monster2.name}`);
+} else {
+    console.log(`${monsters.monster2.name} attacks harder than ${monsters.monster1.name}`);
+}
+
+let monster1Attack = monsters.monster1.attack;
+let monster2Attack = monsters.monster2.attack;
+let monster1Life = monsters.monster1.life;
+let monster2Life = monsters.monster2.life;
+const monster1 = monsters.monster1.name;
+const monster2 = monsters.monster2.name;
+
+if (monster1Attack > monster2Life) {
+    console.log(`${monster2} is dead`);
+} else {
+    console.log(`${monster1} didn't hit as hard and failed to kill ${monster2}.`)
+}
+
 
