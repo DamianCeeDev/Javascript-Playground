57232e05acda68ae2e70fd334fd1ffe02f358c72
DamianCeeDevelops <rdcorbo555@gmail.com>
2024-04-19 23:09:40 -0400

    Expanded small exercises I'm doing with Javascript on here with more lines of code consider this meaningless code but just testing lines of code out.

--- Javascript Practice/test.js
+++ Javascript Practice/test.js
@@ -38,3 +38,54 @@ console.log(monsters.alive);
 
 monsters.powers = "being really smell";
 console.log(monsters);
+
+const country = "USA";
+const continent = "North America";
+let population = 333033000;
+const isIsland = true;
+const language = "English";
+
+console.log(country);
+console.log(continent);
+console.log(population);
+console.log(isIsland);
+console.log(language);
+
+console.log(typeof country);
+console.log(typeof continent);
+console.log(typeof population);
+console.log(typeof isIsland);
+console.log(typeof language);
+
+let halfofcountry = population / 2;
+console.log(halfofcountry);
+
+let increasedPopulation = population + 1;
+console.log(increasedPopulation);
+
+const description = `${country} is in ${continent} and it has a population of ${population} people that speak ${language}.`;
+
+console.log(description);
+
+let finland = 6000000;
+
+if (population > finland) {
+    console.log(country + " has a higher population than Finland.");
+} else {
+    console.log(country + " doesn't have a higher population than Finland.")
+}
+
+console.log("No Conversion");
+console.log('9' - '5'); // -> ?
+console.log('19' - '13' + '17'); // -> ?
+console.log('19' - '13' + 17); // -> ?
+console.log('123' < 57); // -> ?
+console.log(5 + 6 + '4' + 9 - 4 - 2); // -> ?
+
+console.log("Conversion");
+console.log(Number('9') - Number('5')); // -> ?
+console.log(Number('19') - Number('13') + Number('17')); // -> ?
+console.log(Number('19') - Number('13') + 17); // -> ?
+console.log(Number('123') < 57); // -> ?
+console.log(5 + 6 + Number('4') + 9 - 4 - 2); // -> ?
+
