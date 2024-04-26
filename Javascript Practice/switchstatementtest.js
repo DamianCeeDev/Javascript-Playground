d5d27069896909cb9ec0e037a777d492dd077ac6
DamianCeeDevelops <rdcorbo555@gmail.com>
2024-04-25 23:49:13 -0400

    Meanginless code but I was trying out a swtch statement.

---
+++ Javascript Practice/switchstatementtest.js
@@ -0,0 +1,28 @@
+const day = 'Monday';
+
+switch (day) {
+    case 'Sunday':
+        console.log("Day off. Chillin");
+        break;
+    case 'Monday':
+        console.log('Morning Shift');
+        break;
+    case 'Tuesday':
+        console.log('Day off');
+        break;
+    case 'Wednesday':
+        console.log('Evening Shift');
+        break;
+    case 'Thursday':
+        console.log('Evening Shift');
+        break;
+    case 'Friday':
+        console.log('Morning Shift');
+        break;
+    case 'Saturday':
+        console.log('Morning Shift');
+        break;
+    default:
+        console.log('Invalid day');
+}
+
