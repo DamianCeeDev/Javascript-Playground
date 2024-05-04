a31fe428a3f33864dd7bbf16e95416caa92f32ee
DamianCeeDevelops <rdcorbo555@gmail.com>
2024-05-03 21:41:00 -0400

    Improved tipCalculator.js with arrays.

--- Javascript Practice/tipCalculator.js
+++ Javascript Practice/tipCalculator.js
@@ -9,3 +9,24 @@ const totalValue = bill + tip;
 console.log(`The bill was ${bill}, the tip was ${tip} and the total value was ${totalValue}`);
 
 // Test Comment 
+
+// Below is an improved version using arrays as well.
+
+/* Write your code below. Good luck! 🙂 */
+
+function calcTip(bill) {
+    if (bill >= 50 && bill <= 300) {
+        let tip = bill * .15;
+        return tip;
+    } else {
+        let tip = bill * .20;
+        return tip;
+    }
+
+}
+console.log(calcTip(100));
+
+const bills = [125, 255, 44];
+
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+console.log(tips);
