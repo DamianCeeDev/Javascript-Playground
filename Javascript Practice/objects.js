742b66e53e4ffd8c75d380f399ec8a88f4b5af8b
DamianCeeDevelops <rdcorbo555@gmail.com>
2024-05-06 21:59:55 -0400

    Added more code from Jonas' assignment from the object methods section.

--- Javascript Practice/objects.js
+++ Javascript Practice/objects.js
@@ -56,3 +56,36 @@ characterPlayer.favoriteSaying = prompt("What is your character's favorite sayin
 
 console.log(characterPlayer);
 
+// Jonas Assignment below for Function Methods
+
+const mark = {
+    fullName: "Mark Miller",
+    mass: 78,
+    height: 1.69,
+    calcBMI: function () {
+        this.bmi = this.mass / (this.height * this.height);
+        return this.bmi;
+    },
+
+}
+
+const john = {
+    fullName: "John Smith",
+    mass: 92,
+    height: 1.95,
+    calcBMI: function () {
+        this.bmi = this.mass / (this.height * this.height);
+        return this.bmi;
+    },
+}
+
+console.log(mark.calcBMI());
+console.log(john.calcBMI());
+
+if (john.bmi > mark.bmi) {
+    console.log(`${john.fullName}'s BMI(${john.bmi}) is higher than ${mark.fullName}'s (${mark.bmi})`);
+    console.log(`${john.fullName} wins!`)
+} else {
+    console.log(`${mark.fullName}'s BMI(${mark.bmi}) is higher than ${john.fullName}'s (${john.bmi})`);
+    console.log(`${mark.fullName} wins!`)
+}
