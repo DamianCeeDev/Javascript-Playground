c2106d2cbaf3cd51c93ffd7517d13f5a90bfb35f
DamianCeeDevelops <rdcorbo555@gmail.com>
2024-05-11 23:02:29 -0400

    Practicing a mix between For Loops and lists and objects with an added loopPractice.js

---
+++ Javascript Practice/loopPractice.js
@@ -0,0 +1,16 @@
+const whoisthisGuy = {
+    name: "Damian",
+    age: 41,
+    born: `Miami`
+}
+
+for (const key in whoisthisGuy) {
+    console.log(whoisthisGuy[key]);
+}
+
+const listofHobbies = [`music`, `3dart`, `baseball`, `candy eating`, `random stuff`];
+
+for (let haha in listofHobbies) {
+    console.log(listofHobbies[haha]);
+}
+
