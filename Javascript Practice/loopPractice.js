c220abc9d9c56feac9b4df9814854c5b6de7c30f
DamianCeeDevelops <rdcorbo555@gmail.com>
2024-05-12 20:37:42 -0400

    More practicing with for loops

--- Javascript Practice/loopPractice.js
+++ Javascript Practice/loopPractice.js
@@ -14,3 +14,24 @@ for (let haha in listofHobbies) {
     console.log(listofHobbies[haha]);
 }
 
+const listofRandomThings = [`thing 1`, `thing 2`, `thing 3`, `thing 4`, `thing 5`, `thing 6`];
+
+for (let i = 0; i < listofRandomThings.length; i++) {
+    console.log(listofRandomThings[i]);
+}
+
+for (let i = listofRandomThings.length - 1; i >= 0; i--) {
+    console.log(listofRandomThings[i]);
+}
+
+for (let chapter = 1; chapter <= 12; chapter++) {
+    console.log(`--------------------- Chapter ${chapter} --------------------- `);
+    for (let page = 1; page <= 10; page++) {
+        console.log(`****            Page ${page}              ****`);
+        for (let sentence = 0; sentence <= 10; sentence++) {
+            console.log(`This is sentence #${sentence}`)
+        }
+    }
+
+}
+
